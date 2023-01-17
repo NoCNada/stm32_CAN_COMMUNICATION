5af8785b26c0246c6dfee51a564d1fbc69ce1b88
Molina Lautaro <molinalautaro@pm.me>
2023-01-17 19:05:27 -0300

    Anda comunicacion can Recepcion y Transmision

---
+++ Core/Src/main.c
@@ -0,0 +1,263 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "can.h"
+#include "tim.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+CAN_RxHeaderTypeDef rxHeader;
+CAN_TxHeaderTypeDef txHeader;
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define id_tx 0x10 //MAEXTRO
+#define id_rx 0x123 //ESCLAVO
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint8_t CanTxBuffer[8];
+uint8_t CanRxBuffer[8];
+uint32_t CanMailBox;
+uint8_t flag_rx = 0, flag_tx = 0;
+uint8_t counterTime = 5;
+uint8_t count = 0;
+uint8_t nice = 0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+void SystemClock_Config(void);
+void CAN_Filter_Config(void);
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_CAN_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+	CAN_Filter_Config();
+	HAL_CAN_Start(&hcan);
+	HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING);
+  HAL_TIM_Base_Start_IT(&htim2);
+
+	txHeader.DLC = 2;
+    txHeader.IDE = CAN_ID_STD;
+    txHeader.RTR = CAN_RTR_DATA;
+    txHeader.StdId = id_tx;
+
+    CanTxBuffer[0] = 5;
+    CanTxBuffer[1] = 200;
+    //HAL_CAN_AddTxMessage(&hcan, &txHeader, CanTxBuffer, &CanMailBox);
+    //HAL_CAN_IsTxMessagePending(&hcan, CanMailBox);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  if(flag_rx == 1){
+		  counterTime = CanRxBuffer[0];
+		  nice = CanRxBuffer[1];
+		  flag_rx = 0;
+		  flag_tx = 1;
+	  }
+	  if(count == 0){
+		  count = counterTime;
+		  HAL_GPIO_TogglePin(Led_GPIO_Port, Led_Pin);
+		  HAL_GPIO_TogglePin(Led_Onboard_GPIO_Port, Led_Onboard_Pin);
+	  }
+	  if(nice){
+		  txHeader.DLC = 2;
+		  txHeader.IDE = CAN_ID_STD;
+		  txHeader.RTR = CAN_RTR_DATA;
+		  txHeader.StdId = id_tx;
+
+		  CanTxBuffer[0] = counterTime+1;
+		  CanTxBuffer[1] = 200;
+		  HAL_CAN_AddTxMessage(&hcan, &txHeader, CanTxBuffer, &CanMailBox);
+		  HAL_CAN_IsTxMessagePending(&hcan, CanMailBox);
+		  flag_tx = 0;
+		  //counterTime = 1;
+	  }
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	if(count){
+		count--;
+	}
+}
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+	HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rxHeader, CanRxBuffer);
+	if(rxHeader.DLC == 2)
+	{
+		flag_rx = 1;
+	}
+}
+
+void CAN_Filter_Config(void)
+{
+	CAN_FilterTypeDef canfilter;
+
+	canfilter.FilterActivation = CAN_FILTER_ENABLE;
+	canfilter.FilterBank = 10;
+	canfilter.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+	canfilter.FilterIdHigh = (id_rx << 5);
+	canfilter.FilterIdLow = 0x0000;
+	canfilter.FilterMaskIdHigh = (id_rx << 5);
+	canfilter.FilterMaskIdLow = 0x0000;
+	canfilter.FilterMode = CAN_FILTERMODE_IDMASK;
+	canfilter.FilterScale = CAN_FILTERSCALE_32BIT;
+	canfilter.SlaveStartFilterBank = 0;
+
+	HAL_CAN_ConfigFilter(&hcan, &canfilter);
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
